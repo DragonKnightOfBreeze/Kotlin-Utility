bd0a16f39df8026675b0b9285b813f6592a906cd
微风的龙骑士 风游迩 <2944262241@qq.com>
2019-06-01 19:33:50 +0800

    更新snakeYaml依赖，解决NoClassDefFoundException问题

--- src/main/kotlin/module-info.java
+++
@@ -1,8 +0,0 @@
-module Kotlin.Utility {
-	requires kotlin.stdlib;
-	requires snakeyaml;
-	requires gson;
-	requires commons.logging;
-	//为了解决snakeYaml报错ClassDefNotFoundException，必须添加这个依赖
-	requires java.sql;
-}
