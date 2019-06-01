91d2240015758fbb0fef12e12c3f25602e8a60e5
微风的龙骑士 风游迩 <2944262241@qq.com>
2019-06-02 01:57:53 +0800

    解决bug

--- src/main/kotlin/com/windea/kotlin/generator/impl/JsonSchemaGenerator.kt
+++ src/main/kotlin/com/windea/kotlin/generator/impl/JsonSchemaGenerator.kt
@@ -8,6 +8,8 @@ import com.windea.kotlin.generator.ITextGenerator
 import com.windea.kotlin.generator.JsonSchemaRule
 import com.windea.kotlin.utils.JsonUtils
 import com.windea.kotlin.utils.YamlUtils
+import java.util.concurrent.ConcurrentHashMap
+import java.util.concurrent.ConcurrentMap
 
 /**
  * Json Schema的生成器。
@@ -46,7 +48,7 @@ class JsonSchemaGenerator private constructor() : ITextGenerator {
 	
 	override fun execute(): JsonSchemaGenerator {
 		addDefaultRules()
-		convertRules(inputMap)
+		convertRules(dataMap)
 		return this
 	}
 	
@@ -83,19 +85,21 @@ class JsonSchemaGenerator private constructor() : ITextGenerator {
 	
 	//递归遍历整个约束映射的深复制，处理原本的约束映射
 	//如果找到了自定义规则，则替换成规则集合中指定的官方规则
+	//使用并发映射解决java.util.ConcurrentModificationException
 	private fun convertRules(map: MutableMap<String, Any?>) {
-		for(key in map.keys) {
+		val concurrentMap = ConcurrentHashMap(map)
+		for(key in concurrentMap.keys) {
 			//如果值为映射，则继续向下递归遍历，否则检查是否匹配规则名
 			val value = map[key]
 			if(value is MutableMap<*, *>) {
-				convertRules(value as MutableMap<String, Any?>)
+				convertRules(value as ConcurrentMap<String, Any?>)
 			} else {
 				//如果找到了对应规则名的规则，则执行规则并替换
 				ruleMap[key]?.let {
 					val newRule = it.invoke(Pair(key, value))
 					//居然还能直接这样写？
-					map -= key
-					map += newRule
+					concurrentMap -= key
+					concurrentMap += newRule
 				}
 			}
 		}
