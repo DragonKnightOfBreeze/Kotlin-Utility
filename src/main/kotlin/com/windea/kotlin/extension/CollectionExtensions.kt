c13f57a8898a5c53f79ef1e8194f090509046fd5
微风的龙骑士 风游迩 <2944262241@qq.com>
2019-06-02 18:23:01 +0800

    重构代码

--- src/main/kotlin/com/windea/kotlin/extension/CollectionExtensions.kt
+++ src/main/kotlin/com/windea/kotlin/extension/CollectionExtensions.kt
@@ -21,17 +21,19 @@ fun <E> List<E>.query(path: String): List<Any?> = queryInCollection(this, path)
 /**
  * 根据指定路径 [path] 查询当前集合，返回匹配的值列表。
  *
+ * 示例： `#/Data/{Category}/[1]/Name`。
+ *
  * 允许的子路径格式：
  * * `[]` 表示一个列表。
- * * `[1..10]` 表示一个所应在指定范围内的列表。包含上下限。
- * * `[1]` 表示一个列表的索引。
+ * * `1..10` 表示一个所应在指定范围内的列表。包含上下限。
+ * * `1` 表示一个列表的索引。
  * * `{}` 表示一个对象/映射。
  * * `{Category}` 表示一个注为指定占位符的对象/映射。
  * * `re:Name.*` 表示一个属性/键匹配指定正则的对象/映射。
  * * `Name` 表示一个对象/映射的属性/键。
  */
 private fun queryInCollection(collection: Any?, path: String): List<Any?> {
-	val fixedPath = path.trim().removePrefix("/")
+	val fixedPath = path.trim().removePrefix("#").removePrefix("/")
 	val subPaths = fixedPath.split("/")
 	var valueList = listOf(collection)
 	
@@ -41,14 +43,14 @@ private fun queryInCollection(collection: Any?, path: String): List<Any?> {
 			subPath == "[]" -> {
 				valueList.flatMap { it as List<Any?> }
 			}
-			//如果子路径表示一个范围，例如："[1..10]"
-			subPath matches Regex("\\[\\d+\\.\\.\\d+]") -> {
-				val (fromIndex, toIndex) = subPath.removeSurrounding("[", "]").split("..").map { it.toInt() }
+			//如果子路径表示一个范围，例如："1..10"
+			subPath matches Regex("\\d+\\.\\.\\d+") -> {
+				val (fromIndex, toIndex) = subPath.split("..").map { it.toInt() }
 				valueList.flatMap { (it as List<Any?>) }.subList(fromIndex, toIndex + 1)
 			}
-			//如果子路径表示一个列表索引，例如："[1]"
-			subPath matches Regex("\\[\\d+]") -> {
-				val index = subPath.removeSurrounding("[", "]").toInt()
+			//如果子路径表示一个列表索引，例如："1"
+			subPath matches Regex("\\d+]") -> {
+				val index = subPath.toInt()
 				valueList.map { (it as List<Any?>)[index] }
 			}//[1]
 			//如果子路径表示一个对象，例如："{}"
