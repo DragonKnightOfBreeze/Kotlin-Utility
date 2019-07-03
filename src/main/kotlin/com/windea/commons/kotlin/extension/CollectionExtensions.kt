0b9a73921d16534749ce25265ccfd223c7986b45
DragonKnightOfBreeze <2944262241@qq.com>
2019-07-03 20:56:36 +0800

    1.3.5 更新字符串拓展。

--- src/main/kotlin/com/windea/commons/kotlin/extension/CollectionExtensions.kt
+++ src/main/kotlin/com/windea/commons/kotlin/extension/CollectionExtensions.kt
@@ -3,56 +3,27 @@
 package com.windea.commons.kotlin.extension
 
 /**
- * 将当前列表转化成以键为值的映射。
+ * 根据指定路径 [path] 查询当前列表，返回匹配的元素列表。
+ *
+ * 示例： `#/0/Name`
+ *
+ * 允许的子路径格式：
+ * * `[]` 表示一个列表。
+ * * `1..10` 表示一个所应在指定范围内的列表。包含上下限。
+ * * `1` 表示一个列表的索引。
+ * * `{}` 表示一个对象/映射。
+ * * `{Category}` 表示一个注为指定占位符的对象/映射。
+ * * `re:Name.*` 表示一个属性/键匹配指定正则的对象/映射。
+ * * `Name` 表示一个对象/映射的属性/键。
  */
-fun <E> List<E>.toIndexedMap(): Map<String, E> {
-	return this.withIndex().map { (i, e) -> Pair(i.toString(), e) }.toMap()
+fun <E> List<E>.query(path: String): List<Any?> {
+	return queryValue(this, path)
 }
-
-
-/**
- * 得到当前映射中指定路径 [path] 的值。
- */
-fun <K, V> Map<K, V>.getValueByPath(path: String): Any? {
-	val subPaths = path.split(".")
-	var subValue = this as Map<String, Any?>
-	for((index, subPath) in subPaths.withIndex()) {
-		if(index == subPaths.lastIndex) {
-			return subValue[subPath]
-		} else if(subValue[subPath] is Map<*, *>) {
-			subValue = subValue[subPath] as Map<String, Any?>
-		} else {
-			return null
-		}
-	}
-	return null
-}
-
-/**
- * 设置当前映射中指定路径 [path] 的值，如果不存在，则持续向下创建。
- */
-fun <K, V> MutableMap<K, V>.setValueByPath(path: String, value: Any?) {
-	val subPaths = path.split(".")
-	var subValue = this as MutableMap<String, Any?>
-	for((index, subPath) in subPaths.withIndex()) {
-		if(index == subPaths.lastIndex) {
-			subValue[subPath] = value
-		} else if(subValue[subPath] is MutableMap<*, *>) {
-			subValue = subValue[subPath] as MutableMap<String, Any?>
-		} else if(subPath !in subValue) {
-			subValue[subPath] = mutableMapOf<String, Any?>()
-			subValue = subValue[subPath] as MutableMap<String, Any?>
-		} else {
-			return
-		}
-	}
-}
-
 
 /**
  * 根据指定路径 [path] 查询当前映射，返回匹配的值列表。
  *
- * 示例： `#/Data/{Category}/1/Name`。
+ * 示例： `#/{Category}/{Name}/Name`。
  *
  * 允许的子路径格式：
  * * `[]` 表示一个列表。
@@ -128,13 +99,21 @@ fun <K, V> Map<K, V>.deepFlatMap(): Map<String, Any?> {
 private fun deepFlatMap(map: Map<String, Any?>, prePaths: MutableList<String>): Map<String, Any?> {
 	return map.flatMap { (key, value) ->
 		prePaths += key
-		if(value is Map<*, *>) {
-			deepFlatMap(value as Map<String, Any?>, prePaths).toList()
-		} else if(value is List<*>) {
-			deepFlatMap(value.toIndexedMap(), prePaths).toList()
-		} else {
-			val fullPath = prePaths.joinToString(".").replace(Regex("\\.(\\d*)\\."), "[$1].")
-			listOf(Pair(fullPath, value))
+		when(value) {
+			is Map<*, *> -> deepFlatMap(value as Map<String, Any?>, prePaths).toList()
+			is List<*> -> deepFlatMap(value.toIndexedMap(), prePaths).toList()
+			else -> {
+				val fullPath = prePaths.joinToString(".").replace(Regex("\\.(\\d*)\\."), "[$1].")
+				listOf(Pair(fullPath, value))
+			}
 		}
 	}.toMap()
+}
+
+
+/**
+ * 将当前列表转化成以键为值的映射。
+ */
+fun <E> List<E>.toIndexedMap(): Map<String, E> {
+	return this.withIndex().map { (i, e) -> Pair(i.toString(), e) }.toMap()
 }
