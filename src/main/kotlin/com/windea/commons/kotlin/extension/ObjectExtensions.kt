b6b35c5e1e768a0d27b3003712b13e32d0c6cd46
DragonKnightOfBreeze <2944262241@qq.com>
2019-07-09 21:59:38 +0800

    1.3.10 更新对象到映射拓展。

--- src/main/kotlin/com/windea/commons/kotlin/extension/ObjectExtensions.kt
+++ src/main/kotlin/com/windea/commons/kotlin/extension/ObjectExtensions.kt
@@ -7,6 +7,6 @@ import java.io.Serializable
  * 将对象转化为映射。不递归转化。
  */
 @NotTested("递归转化")
-fun <T : Serializable> T.toMap(): Map<String, *> {
+fun <T : Serializable> T.toMap(): Map<String, Any?> {
 	return this.javaClass.getGetterMap().mapValues { (_, v1) -> v1.invoke(this) }.filterKeys { it != "class" }
 }
