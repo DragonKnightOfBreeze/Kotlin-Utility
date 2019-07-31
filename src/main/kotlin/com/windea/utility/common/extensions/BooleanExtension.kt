6e536ee839d13244260bcf211932a0d5495db05e
DragonKnightOfBreeze <2944262241@qq.com>
2019-07-31 11:28:35 +0800

    1.4.2 更新集合扩展。

--- src/main/kotlin/com/windea/utility/common/extensions/BooleanExtension.kt
+++ src/main/kotlin/com/windea/utility/common/extensions/BooleanExtension.kt
@@ -1,16 +1,17 @@
 package com.windea.utility.common.extensions
 
 /**转化为指定的正数或负数。*/
-fun Boolean.toAbs(value: Int) = if(this) value else -value
+fun Boolean.toOps(value: Int) = if(this) value else -value
 
 /**转化为指定的正数或负数。*/
-fun Boolean.toAbs(value: Float) = if(this) value else -value
+fun Boolean.toOps(value: Float) = if(this) value else -value
 
 /**转化为指定的正数或负数。*/
-fun Boolean.toAbs(value: Double) = if(this) value else -value
+fun Boolean.toOps(value: Double) = if(this) value else -value
+
 
 /**转化为1或0。*/
 fun Boolean.to01() = if(this) 1 else 0
 
 /**转化为1或-1。*/
-fun Boolean.toAbs1() = if(this) 1 else -1
+fun Boolean.toOps1() = if(this) 1 else -1
--- src/main/kotlin/com/windea/utility/common/extensions/NumberExtention.kt
+++ src/main/kotlin/com/windea/utility/common/extensions/NumberExtention.kt
@@ -9,11 +9,31 @@ fun Int.pow(n: Int) = this.toFloat().pow(n).toInt()
 fun Int.pow(n: Float) = this.toFloat().pow(n).toInt()
 
 
-/**限制在指定的绝对值之间。*/
-fun Float.coerceInAbs(value: Float) = this.coerceIn(-value, value)
+/**取当前数值在指定范围内的夹值。*/
+infix fun Int.clamp(range: ClosedRange<Int>) = this.coerceIn(range)
 
-/**限制在指定的绝对值之间。*/
-fun Double.coerceInAbs(value: Double) = this.coerceIn(-value, value)
+/**取当前数值在指定范围内的夹值。*/
+infix fun Long.clamp(range: ClosedRange<Long>) = this.coerceIn(range)
+
+/**取当前数值在指定范围内的夹值。*/
+infix fun Float.clamp(range: ClosedRange<Float>) = this.coerceIn(range)
+
+/**取当前数值在指定范围内的夹值。*/
+infix fun Double.clamp(range: ClosedRange<Double>) = this.coerceIn(range)
+
+
+/**限制在指定的相反数之间。*/
+fun Int.coerceInOps(value: Int) = this.coerceIn(-value, value)
+
+/**限制在指定的相反数之间。*/
+fun Long.coerceInOps(value: Long) = this.coerceIn(-value, value)
+
+/**限制在指定的相反数之间。*/
+fun Float.coerceInOps(value: Float) = this.coerceIn(-value, value)
+
+/**限制在指定的相反数之间。*/
+fun Double.coerceInOps(value: Double) = this.coerceIn(-value, value)
+
 
 /**限制在指定0到1之间。*/
 fun Float.coerceIn01() = this.coerceIn(0f, 1f)
@@ -22,7 +42,7 @@ fun Float.coerceIn01() = this.coerceIn(0f, 1f)
 fun Double.coerceIn01() = this.coerceIn(0.0, 1.0)
 
 /**限制在-1到1之间。*/
-fun Float.coerceInAbs1() = this.coerceIn(-1f, 1f)
+fun Float.coerceInOps1() = this.coerceIn(-1f, 1f)
 
 /**限制在-1到1之间。*/
-fun Double.coerceInAbs1() = this.coerceIn(-1.0, 1.0)
+fun Double.coerceInOps1() = this.coerceIn(-1.0, 1.0)
